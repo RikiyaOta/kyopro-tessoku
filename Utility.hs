d4334772cd115db526e358ee2c0296332af155bf
rikiyaota <rikiya0421.fcb6@gmail.com>
2023-08-13 11:34:28 +0900

    Utility

--- Utility.hs
+++ Utility.hs
@@ -1,12 +1,18 @@
 import Control.Monad
 import Data.Maybe
 import qualified Data.ByteString.Char8 as BS
-
+ 
 readInt :: BS.ByteString -> Int
 readInt = fst . fromJust . BS.readInt
-
 readIntList :: BS.ByteString -> [Int]
 readIntList = map readInt . BS.words
-
+getInt :: IO Int
+getInt = readInt <$> BS.getLine
 getInts :: IO [Int]
 getInts = readIntList <$> BS.getLine
+getIntNLists :: Int -> IO [[Int]]
+getIntNLists n = fmap readIntList <$> replicateM n BS.getLine 
+getIntMatrix :: IO [[Int]]
+getIntMatrix = fmap readIntList . BS.lines <$> BS.getContents
+getCharNLists :: Int -> IO [[Char]]
+getCharNLists n = fmap BS.unpack <$> replicateM n BS.getLine
